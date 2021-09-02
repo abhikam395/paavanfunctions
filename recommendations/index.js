20067ac93edbf4ae63c2f2b6383bccd79aba71eb
abhikam395 <abhikam395@gmail.com>
2021-09-02 19:21:44 +0530

    recommendation completed

--- recommendations/index.js
+++ recommendations/index.js
@@ -1,10 +1,28 @@
-const {videos} = require('./videos');
+const {database, admin} = require('./firebaseDatabase');
 
-exports.recommendations = function(req, res){
-    let {v} = req.query;
+const entityCollection = database.collection("entities");
+
+function parseDocs(docs){
+    return docs.map(document =>  {
+      let [id, data] = [document.id, document.data()];
+      return {id, ...data};
+    });
+}  
+
+exports.recommendations = async function(req, res){
+    let {tags, entityId} = req.body;
+    let parsedTags = JSON.parse(tags);
+
+    const snapshot = await 
+        entityCollection
+            .where(admin.firestore.FieldPath.documentId(), '!=', entityId)
+            .where('tags', 'array-contains-any', parsedTags)
+            .get();
+
+    const entities = parseDocs(snapshot.docs);
 
     res.status(200).json({
         status: true,
-        data: videos(v)
+        data: entities
     });
 }
